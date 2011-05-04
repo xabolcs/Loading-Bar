b067252e56a40dbbea2fe22d35e9d4d993b0ffa2
Mattias Lindholm <mattias@xertoz.se>
2011-05-04 16:35:28 +0200

    Initial commit for the git repository.

---
+++ chrome/content/ff-overlay.js
@@ -0,0 +1,26 @@
+var LoadingBar = {
+    listener: {
+        onChangeTab: function(e) {
+            document.getElementById('urlbar').completed = 0;
+        },
+        
+        onProgressChange: function(aBrowser,webProgress,request,curSelfProgress,maxSelfProgress,curTotalProgress,maxTotalProgress) {
+            if (gBrowser.contentDocument === aBrowser.contentDocument)
+                document.getElementById('urlbar').completed = curTotalProgress/maxTotalProgress;
+        },
+        
+        onStateChange: function() {
+            document.getElementById('urlbar').completed = 0;
+        },
+        
+        onLocationChange: function() {},
+        onSecurityChange: function() {},
+        onStatusChange: function() {}
+    }
+};
+
+// Hook events when the addon finishes loading
+window.addEventListener('load',function() {
+    gBrowser.tabContainer.addEventListener('TabSelect',LoadingBar.listener.onChangeTab,false);
+    gBrowser.addTabsProgressListener(LoadingBar.listener);
+},false);
